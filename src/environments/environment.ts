8be08c46c4147d576ce73fe6cb5669ab1dd69d00
Alexander Martinez <alex95mf@gmail.com>
2025-06-18 22:41:51 -0500

    [NEW] Correcciones 45: Api url de ambiente de pruebas (y no localhost) en environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@ export const environment = {
   apiUrl: 'https://recursos.aptimosoft.com/api/v1/',
   baseUrl: 'https://recursos.aptimosoft.com', */
 
-  // apiUrl: 'http://api-contable.local:8088/api/v1/',
-  // baseUrl: 'http://api-contable.local:8088',
-  apiUrl: 'http://localhost:8000/api/v1/',
-  baseUrl: 'http://localhost:8000',
+  apiUrl: 'http://api-contable.local:8088/api/v1/',
+  baseUrl: 'http://api-contable.local:8088',
+  // apiUrl: 'http://localhost:8000/api/v1/',
+  // baseUrl: 'http://localhost:8000',
   // apiUrl: 'https://gad-api.aptimosoft.com/api/v1/',
   // baseUrl: 'https://gad-api.aptimosoft.com',
   apiSocket: 'http://localhost:3001/',
